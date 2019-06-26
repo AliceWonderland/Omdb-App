299583e0f7e2d35dc8441b74ae14821857cc69d1
Alice Chuang <bomberetta@gmail.com>
2019-06-26 00:44:10 -0400

    cleanup

--- client/components/Main.js
+++ client/components/Main.js
@@ -13,8 +13,23 @@ class Main extends Component {
 		return (
 		  <main>
 			  <h1>HOME</h1>
-			  <i class="fas fa-camera-retro"></i>
-			  <i class="fas fa-user"></i>
+			  <p>mov·ie
+				/ˈmo͞ovē/
+
+				a story or event recorded by a camera as a set of moving images and shown in a theater or on television; a motion picture.
+				synonyms:	film, picture, motion picture, feature, feature film; More
+				a movie theater.
+
+				plural noun: the movies
+				"we decided to go to the movies"
+				motion pictures generally or the motion-picture industry.
+
+				plural noun: the movies
+				"a lifelong love of the movies"
+				
+				synonyms:	the cinema, the pictures, the silver screen, the big screen; informalthe flicks
+				"the growth of the movies as mass entertainment"
+				</p>
 		  </main>
 		);
 	}
