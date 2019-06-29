098deb285b1619a5d05485da9ccef779b8897764
Alice Chuang <bomberetta@gmail.com>
2019-06-29 19:32:09 -0400

    header styles

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Nav } from '../components';
 
 class Header extends Component {
 	render() {
 		return (
-		  <header className="App-header">
-			  <i className="fas fa-thumbs-up fa-5x"></i>
-			  <h1>Hello, OMDB!</h1>
-			  <Nav/>
-		  </header>
+            <header className="App-header">
+                <Link to="/">
+                    <i className="fas fa-thumbs-up fa-5x"></i>
+                    <h1>Hello, OMDB!</h1>
+                </Link>
+                <Nav/>
+            </header>
 		);
 	}
 }
